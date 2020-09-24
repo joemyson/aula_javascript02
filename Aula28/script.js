f535c709b8a5e8bd7b62ed423b3fbd37d580f147
joemyson-santos <joemyson1402@gmail.com>
2020-09-24 16:07:36 -0300

    função02

--- Aula28/script.js
+++ Aula28/script.js
@@ -5,14 +5,52 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
+
+
+function numero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+
+        /*esta função vai verifica se o valor informado e um 'numero' e esta entre 1 e 100*/
+        return true
+
+
+
+    } else {
+        return false
+    }
+
+}
+function islista(n, l) {
+
+    /*realizar busca de numero */
+
+    if (l.indexOf(Number(n)) != -1) {
+
+
+        return true
+
+
+    } else {
+        return false
+    }
+
+
+}
+
+
+
+
+
+
+
 function adcionar() {
 
-    if (numero(num.value) && !inlista(num.value)) {
+    if (numero(num.value) && !inlista(num.value, valores)) {
 
         /*atributo 'função numero() recebendo valor  (num) e diferente da função inlista()*/
 
 
-
+        window.alert('tudo ok')
 
 
 
